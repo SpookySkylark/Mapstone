4f129372940eb7c47986f1749aab70a82d4ff780
SpookySkylark <64929944+SpookySkylark@users.noreply.github.com>
2021-10-20 11:06:29 -0700

    Delete HeaderComponentLogin.js

--- Mapstone/app/components/HeaderComponentLogin.js
+++
@@ -1,7 +0,0 @@
-import { Jumbotron } from 'react-bootstrap/';
-
-const HeaderComponent = ({className}) => {
-    return (<Jumbotron><h1 className={className}>Camosun Alumni Career Map</h1></Jumbotron>);
-}
-
-export default HeaderComponent;
